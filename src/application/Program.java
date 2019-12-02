678d6de9e8f9e336c9e4fe225021f28a633ee45d
WEVERTON JOAQUIM AVELINO <WAVELINO@bbmapfre.com.br>
2019-12-02 07:49:13 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,29 @@
 package application;
 
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		ChessMatch chesMatch = new ChessMatch();
-		UI.printBoard(chesMatch.getPieces());
-	}
 
+		Scanner sc = new Scanner(System.in);
+		ChessMatch chesMatch = new ChessMatch();
+
+		while (true) {
+			UI.printBoard(chesMatch.getPieces());
+			System.out.println();
+			System.out.print("Souce: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			
+			System.out.println();
+			System.out.print(" Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chesMatch.perfomrChessMove(source, target);
+		}
+	}
 }
